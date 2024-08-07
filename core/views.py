8ce11108ed91c9799edc1b123c27405a877bfc33
katsuyorii <katsuyoriiii@gmail.com>
2024-08-07 16:12:09 +0300

    Add static files, create about view and template

--- core/views.py
+++ core/views.py
@@ -4,11 +4,23 @@ from .models import SliderImage
 
 
 class IndexView(TemplateView):
+    """ Представление для главной страницы сайта """
     template_name = 'core/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Платформа онлайн обучения katsu.dev'
         context['slider_images'] = SliderImage.objects.all()
+
+        return context
+    
+
+class AboutView(TemplateView):
+    """ Представление для страницы сайта - «О проекте» """
+    template_name = 'core/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'О проекте'
 
         return context
