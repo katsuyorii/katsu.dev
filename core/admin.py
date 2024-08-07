6dfe03d6d0cda5276ff7967872a35090408a92c9
katsuyorii <katsuyoriiii@gmail.com>
2024-08-07 20:14:44 +0300

    Change register in admin panel models

--- core/admin.py
+++ core/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import SliderImage, Feedback
 
 
-admin.site.register(SliderImage)
-admin.site.register(Feedback)
+@admin.register(SliderImage)
+class SliderImageAdmin(admin.ModelAdmin):
+    list_display = ['image', 'alt_text']
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'message', 'created_date']
+    date_hierarchy = 'created_date'
--- courses/admin.py
+++ courses/admin.py
@@ -3,15 +3,33 @@ from django.contrib import admin
 from .models import Course, Theme, Lesson, LessonMaterial, LessonQuestion
 
 
+""" Переопределение пустого поля на кастомное """
+admin.AdminSite.empty_value_display = "---пусто---"
+
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount_students', 'amount_hours', 'price', 'discount', 'status']
     prepopulated_fields = {'slug': ['title']}
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
+    list_display = ['name']
     prepopulated_fields = {'slug': ['name']}
 
-admin.site.register(Theme)
-admin.site.register(LessonMaterial)
-admin.site.register(LessonQuestion)
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+@admin.register(LessonMaterial)
+class LessonMaterialAdmin(admin.ModelAdmin):
+    list_display = ['file', 'extension']
+
+
+@admin.register(LessonQuestion)
+class LessonQuestionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'answer']
+
