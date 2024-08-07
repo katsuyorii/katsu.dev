1d39b37b66e3010d0a7b3d0b6c64942d8a83b693
katsuyorii <katsuyoriiii@gmail.com>
2024-08-07 15:59:35 +0300

    Add processing to images debug mode and change index view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from .models import SliderImage
+
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
@@ -7,5 +9,6 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Платформа онлайн обучения katsu.dev'
+        context['slider_images'] = SliderImage.objects.all()
 
         return context
