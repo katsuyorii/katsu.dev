142b1acc2c67566ed222fcd6f1a986a499fbfe2c
katsuyorii <katsuyoriiii@gmail.com>
2024-08-10 15:50:10 +0300

    Create grade, category and questions models for training

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Grade, Category, QuestionTraining
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(QuestionTraining)
+class QuestionTrainingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'answer', 'grade']
--- training/models.py
+++ training/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+from django_ckeditor_5.fields import CKEditor5Field
+
+
+class Grade(models.Model):
+    """ Модель уровня вопроса по градации опыта и скилла (Traine, Junior, Middle, Senior) """
+    name = models.CharField(verbose_name='Наименование', max_length=128, unique=True, db_index=True)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Уровень скилла и опыта'
+        verbose_name_plural = 'Уровни скилла и опыта'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """ Модель категории вопроса (Python, Web, основы и т.д.) """
+    name = models.CharField(verbose_name='Наименование', max_length=128, unique=True, db_index=True)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+    
+
+class QuestionTraining(models.Model):
+    """ Модель вопроса в тренажере """
+    title = models.CharField(verbose_name='Наименование вопроса', max_length=128, unique=True, db_index=True)
+    answer = CKEditor5Field(verbose_name='Ответ на вопрос', config_name='default')
+    grade = models.ForeignKey(verbose_name='Уровень скилла и опыта', to=Grade, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(verbose_name='Категории', to=Category)
+
+    class Meta:
+        verbose_name = 'Вопрос тренажера'
+        verbose_name_plural = 'Вопросы тренажера'
+
+    def __str__(self):
+        return self.title
+
