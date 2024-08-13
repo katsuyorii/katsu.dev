4f279c4fe39b3e8127146c4ef1c9a6a042675966
katsuyorii <katsuyoriiii@gmail.com>
2024-08-13 16:54:29 +0300

    Create registration form, url and view

--- authorization/forms.py
+++ authorization/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+from handlers.validators import ban_words_validator
 
 
 class LoginForm(forms.Form):
@@ -24,3 +27,45 @@ class LoginForm(forms.Form):
         
         self.cleaned_data['user'] = user
         return self.cleaned_data
+    
+
+class RegistrationForm(forms.Form):
+    """ Форма регистрации новых пользователей """
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'main-login-auth-block-input', 
+        'placeholder': 'Введите email',
+    }))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'main-login-auth-block-input', 
+        'placeholder': 'Введите никнейм',
+    }), validators=[ban_words_validator])
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs= {
+        'class': 'main-login-auth-block-input', 
+        'placeholder': 'Введите пароль',
+    }), validators=[validate_password])
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs= {
+        'class': 'main-login-auth-block-input', 
+        'placeholder': 'Повторите пароль',
+    }), validators=[validate_password])
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Введенные пароли отличаются!')
+        
+        return password2
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        user_model = get_user_model()
+
+        if user_model.objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким email уже существует!')
+        
+        return email
--- authorization/views.py
+++ authorization/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 class LoginView(FormView):
@@ -28,3 +28,33 @@ class LoginView(FormView):
         context['title'] = 'Авторизация'
 
         return context
+    
+
+class RegistrationView(FormView):
+    """ Представление для страницы регистрации новых пользователей """
+    form_class = RegistrationForm
+    template_name = 'authorization/registration.html'
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+    def form_valid(self, form): 
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password1 = form.cleaned_data['password1']
+
+        user_model = get_user_model()
+
+        user = user_model.objects.create_user(username=username, email=email, password=password1, is_active=True)
+        login(self.request, user)
+        
+        return super().form_valid(form)
+            
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['title'] = 'Регистрация'
+
+            return context
