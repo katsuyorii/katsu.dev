7f0ecee4c3e6ab45486798783e7a7dc438b6a5c9
katsuyorii <katsuyoriiii@gmail.com>
2024-09-19 09:48:39 +0300

    Remove cache for course detail page and change template

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_page
 
 from .views import CoursesListView, CourseDetailView, LessonDetailView
 
 
 urlpatterns = [
-    path('', cache_page(60*15)(CoursesListView.as_view()), name='courses_list'),
-    path('<slug:course_slug>/', cache_page(60*15)(CourseDetailView.as_view()), name='course_detail'),
-    path('<slug:course_slug>/lessons/<slug:lesson_slug>', cache_page(60*15)(login_required(LessonDetailView.as_view())), name='lesson_detail'),
+    path('', CoursesListView.as_view(), name='courses_list'),
+    path('<slug:course_slug>/', CourseDetailView.as_view(), name='course_detail'),
+    path('<slug:course_slug>/lessons/<slug:lesson_slug>', login_required(LessonDetailView.as_view()), name='lesson_detail'),
 ]
