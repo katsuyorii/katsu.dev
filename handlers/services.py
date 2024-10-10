823b174b4a401aef0dbd0c57f7b67cb946daad82
katsuyorii <katsuyoriiii@gmail.com>
2024-10-10 22:16:04 +0300

    Add settings for ckeditor5, fix color ckeditor admin panel and create custom file storage for upload images ckeditor

--- handlers/services.py
+++ handlers/services.py
@@ -1,6 +1,16 @@
 import json
+import os
+
+from urllib.parse import urljoin
 
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class CustomStorage(FileSystemStorage):
+    """ Кастомная реализация хранения файлов у CKEditor5 """
+    location = os.path.join(settings.MEDIA_ROOT, "ckeditor_upload_files")
+    base_url = urljoin(settings.MEDIA_URL, "ckeditor_upload_files/")
 
 
 def get_banned_words():
