2cb9c1a6dbca2f3f5d9fd09656c63ee9f9e1c22e
katsuyorii <katsuyoriiii@gmail.com>
2024-10-14 12:58:55 +0300

    Create training list search template, view and url and change form search for training

--- training/urls.py
+++ training/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TrainingListView, TrainingGradeListView, TrainingCategoriesListView
+from .views import TrainingListView, TrainingGradeListView, TrainingCategoriesListView, TrainingListSearchView
 
 
 urlpatterns = [
     path('', TrainingListView.as_view(), name='training_list'),
     path('grade/<slug:grade_slug>/', TrainingGradeListView.as_view(), name='grade_list'),
+    path('search/', TrainingListSearchView.as_view(), name='search_training'),
     path('categories/<slug:category_slug>/', TrainingCategoriesListView.as_view(), name='category_list'),
 ]
--- training/views.py
+++ training/views.py
@@ -61,3 +61,31 @@ class TrainingCategoriesListView(ListView):
         context['grades'] = Grade.objects.all()
 
         return context
+    
+
+class TrainingListSearchView(ListView):
+    """ Представление для поиска вопросов по названию """
+    model = QuestionTraining
+    template_name = 'training/training_list_search.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        filter_value = self.request.GET.get('filter')
+
+        if filter_value == "":
+            queryset = QuestionTraining.objects.none()
+        else:
+            queryset = QuestionTraining.objects.filter(title__icontains=filter_value).select_related('grade').prefetch_related('categories')
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Тренажер'
+        context['categories'] = Category.objects.all()
+        context['grades'] = Grade.objects.all()
+
+        filter_value = self.request.GET.get('filter')
+        context['filter_value'] = filter_value
+
+        return context
